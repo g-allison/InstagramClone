aebe5c981329339e13cfa3de896c8ee766f59769
g-allison <gallison@fb.com>
2021-07-06 19:26:10 -0400

    Log out with button

--- app/src/main/java/com/codepath/instagramclone/MainActivity.java
+++ app/src/main/java/com/codepath/instagramclone/MainActivity.java
@@ -2,13 +2,41 @@ package com.codepath.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.codepath.instagramclone.databinding.ActivityLoginBinding;
+import com.codepath.instagramclone.databinding.ActivityMainBinding;
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogOff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        View view = binding.getRoot();
+        setContentView(view);
+
+        btnLogOff = binding.btnLogOff;
+        btnLogOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                goLoginActivity();
+            }
+        });
+    }
+
+    private void goLoginActivity() {
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+        finish();
     }
 }
