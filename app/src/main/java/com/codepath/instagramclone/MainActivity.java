e2f5ef0ec27fed0e33db9c7c200c404140a84d08
g-allison <gallison@fb.com>
2021-07-06 21:47:40 -0400

    logout added to toolbar

--- app/src/main/java/com/codepath/instagramclone/MainActivity.java
+++ app/src/main/java/com/codepath/instagramclone/MainActivity.java
@@ -4,16 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 
-import com.codepath.instagramclone.databinding.ActivityLoginBinding;
 import com.codepath.instagramclone.databinding.ActivityMainBinding;
 import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button btnLogOff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,20 +21,29 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
-        btnLogOff = binding.btnLogOff;
-        btnLogOff.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ParseUser.logOut();
-                ParseUser currentUser = ParseUser.getCurrentUser();
-                goLoginActivity();
-            }
-        });
     }
 
     private void goLoginActivity() {
         Intent i = new Intent(this, LoginActivity.class);
         startActivity(i);
         finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.btn_logout) {
+            ParseUser.logOut();
+            ParseUser currentUser = ParseUser.getCurrentUser();
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
